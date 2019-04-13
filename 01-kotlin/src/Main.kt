74344fcd08827c0d2a3c987769fbc64f404160fd
Jacinto Palma <jxcinto12@gmail.com>
2019-04-12 22:05:36 -0500

    Kotlin 1

---
+++ 01-kotlin/src/Main.kt
@@ -0,0 +1,25 @@
+fun main(args : Array<String>){
+    /*
+    *Comentario
+     */
+
+    // Variables
+    //QUE ES MUTAR?? CAMBIAR??
+    //Mutables
+    var nombre = "Jacinto"
+    nombre = "Andres"
+    //Inmutables
+    val nombreI = "Jacinto"
+    //nombreI = "Andres"
+
+    //Tipos de Datos
+    val apellido: String = "Palma"
+    val edad: Int = 25
+    val sueldo: Double = 1.21
+    val casado = false
+    val profesor= true
+    val hijos = null
+
+    apellido
+
+}
