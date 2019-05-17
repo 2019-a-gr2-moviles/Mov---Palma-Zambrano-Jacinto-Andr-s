b49a94f50f4e265c5a7725a588f7b5bff2cdab66
Jacinto Palma <jxcinto12@gmail.com>
2019-05-17 11:49:37 -0500

    INTENT- APP

---
+++ MyApplication2/app/src/main/java/com/example/dr/myapplication/Actividad2.kt
@@ -0,0 +1,33 @@
+package com.example.dr.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_actividad2.*
+
+class Actividad2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad2)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        btn_ActividadUno.setOnClickListener { irAActividadUno() }
+
+    }
+
+    fun irAActividadUno(){
+
+        val intent= Intent(
+            this,
+            MainActivity::class.java
+        )
+        startActivity(intent)
+    }
+}
